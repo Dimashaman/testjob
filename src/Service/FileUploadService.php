bec41056d83cb098064747533268e7f05dbafd5f
Dmitry Shmanko <dimashaman@icloud.com>
2021-11-01 19:40:17 +1100

    FileUploadService

---
+++ src/Service/FileUploadService.php
@@ -0,0 +1,32 @@
+<?php
+namespace App\Service;
+
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+
+class FileUploadService
+{
+    public function __construct(ParameterBagInterface $params)
+    {
+        $this->params = $params;
+    }
+
+    public function uploadAndReturnPath(UploadedFile $file) : string
+    {
+        $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+        $safeFilename = transliterator_transliterate('Any-Latin; Latin-ASCII; [^A-Za-z0-9_] remove; Lower()', $originalFilename);
+        $newFilename = $safeFilename.'-'.uniqid().'.'.$file->guessExtension();
+
+        try {
+            $file->move(
+                $this->params->get('bookcovers_directory'),
+                $newFilename
+            );
+        } catch (FileException $e) {
+            throw $e;
+        }
+
+        return $newFilename;
+    }
+}
